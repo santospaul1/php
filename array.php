118e24836d8d52b9152e5968eb1ebaf6c1477f97
SantosPaulAmiani <107258472+SantosPaulAmiani@users.noreply.github.com>
2023-06-26 19:01:28 +0300

    Add files via upload

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+$cars = array("Benz","BMW","Mitsubishi","Toyota");
+$arraylength = count($cars);
+
+for($x = 0; $x <= $arraylength; $x++){
+    echo $cars[$x];
+    echo "<br />";
+}
+?>
---
+++ associativearray.php
@@ -0,0 +1,4 @@
+<?php 
+$age = array("Peter" => "35", "Ben"=> "32","Lamar"=> "99");
+echo "Lamar is: " .$age['Lamar']. " years old";
+?>
